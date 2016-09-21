7f8acab0231f16d87432836f1615fda49a76383e
Mikko Pakkanen <mikkopak@gmail.com>
2016-09-21 03:40:14 +0300

    Tehtava 2 - Lotto valmis

---
+++ Tehtava 2 - Lotto/index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server">Rivien lukumäärä</asp:Label>
+        <asp:DropDownList id="rivit" runat="server" />
+        <br />
+        <asp:Label runat="server">Pelityyppi</asp:Label>
+        <asp:DropDownList ID="tyyppi" runat="server">
+            <asp:ListItem Text="Lotto" Value="Lotto"></asp:ListItem>
+            <asp:ListItem Text="ViikingLotto" Value="ViikingLotto"></asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:Label runat="server" ID="showResults"></asp:Label>
+        <br />
+        <asp:Button ID="haeRivit" Text="Tee Rivit" runat="server" OnClick="haeRivit_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Tehtava 2 - Lotto/index.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebForm1 : System.Web.UI.Page
+{
+    BLLotto blotto = new BLLotto();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (rivit.Items.Count == 0)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                rivit.Items.Add(new ListItem(Convert.ToString(i)));
+            }
+        }
+    }
+
+    protected void haeRivit_Click(object sender, EventArgs e)
+    {
+        if (tyyppi.SelectedValue == "Lotto")
+        {
+            blotto.ArvoRivi(1, Convert.ToInt32(rivit.SelectedValue));
+            foreach (var list in blotto.drawnNumbers)
+            {
+                foreach (var number in list)
+                {
+                    showResults.Text = showResults.Text + " " + number;
+                }
+                showResults.Text = showResults.Text + "<br />";
+            }
+
+        }
+        else if (tyyppi.SelectedValue == "ViikingLotto")
+        {
+            blotto.ArvoRivi(2, Convert.ToInt32(rivit.SelectedValue));
+            foreach (var list in blotto.drawnNumbers)
+            {
+                foreach (var number in list)
+                {
+                    showResults.Text = showResults.Text + " " + number;
+                }
+                showResults.Text = showResults.Text + "<br />";
+            }
+        }
+    }
+}
