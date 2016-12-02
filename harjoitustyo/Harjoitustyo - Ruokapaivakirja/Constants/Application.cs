6f6d8ba1f6eff8d6003f5c59eaf785ed8e3e3f61
Mikko Pakkanen <mikkopak@gmail.com>
2016-12-02 20:51:52 +0200

    Harjoitustyo - initial commit

---
+++ harjoitustyo/Harjoitustyo - Ruokapaivakirja/Constants/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace Harjoitustyo___Ruokapaivakirja.Constants
+{
+    public class Application
+    {
+        public const string Name = "Ruokapäiväkirja";
+        public const string ShortName = "Ruokapäiväkirja";
+    }
+}
---
+++ harjoitustyo/Harjoitustyo - Ruokapaivakirja/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Harjoitustyo___Ruokapaivakirja.MvcApplication" Language="C#" %>
